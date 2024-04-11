31118c488f3e828b12c835ce7de333a77b394588
Channon Zuo <channon.z2020@gmail.com>
2024-04-10 22:32:41 -0700

    Updated profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import profileImage from '../assets/default_pfp.png'; // Importing the profile image
 import { NavigationBar } from '../components/NavigationBar';
 
 export function Profile() {
@@ -9,16 +10,21 @@ export function Profile() {
     };
 
     return (
-        <main className='h-screen bg-black'>
+        <main className='h-screen bg-black pt-20'>
             <NavigationBar />
-            <h1 className='bg-black text-center w-auto text-3xl font-bold'>
-                User Profile
-            </h1>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <div className="profile-info">
-                    <h2>Display Name: {user.displayName}</h2>
-                    <h2>First Name: {user.firstName}</h2>
-                    <h2>Last Name: {user.lastName}</h2>
+            <div className="profile-container" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h1 className='bg-black text-3xl font-bold'>
+                        User Profile
+                    </h1>
+                </div>
+                <div className="profile-info" style={{ display: "flex", flexDirection: "row", alignItems: "center", padding: "20px" }}>
+                    <img src={profileImage} alt="Profile" style={{ width: "150px", height: "150px", borderRadius: "50%", marginRight: "20px" }} />
+                    <div>
+                        <h2>Display Name: {user.displayName}</h2>
+                        <h2>First Name: {user.firstName}</h2>
+                        <h2>Last Name: {user.lastName}</h2>
+                    </div>
                 </div>
             </div>
         </main>
