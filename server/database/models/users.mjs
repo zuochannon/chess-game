df052e1458ee32512ace5f338e221d93c97c7a96
Azlothe <bbqian7@gmail.com>
2024-05-03 21:20:56 -0700

    bugfix: fix duplicate key error through override on conflict

---
+++ server/database/models/users.mjs
@@ -0,0 +1,25 @@
+import bcrypt from "bcryptjs";
+import { cassandraClient, pool } from "../connection.mjs";
+import constants from "../constants.mjs";
+
+const saltRounds = 10;
+
+export const insertUser = async (username, email, password) => {
+    // we do not assume the caller has hashed the password; we assume the caller may have passed in plaintext
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const userAuthResult = await pool.query(
+      `INSERT INTO Users (userID, username, email, password) VALUES
+      (uuid_generate_v4(), $1, $2, $3) ON CONFLICT (username) DO NOTHING RETURNING userID;`,
+      [username, email, hashedPassword]
+    );
+
+    const uuid = userAuthResult.rows[0]?.userid;
+    
+    if (uuid) {
+        const query = `INSERT INTO ${constants.KEYSPACE}.Users (userID, username, email) VALUES (?, ?, ?);`;
+        await cassandraClient.execute(query, [uuid, username, email], { prepare: true });
+    }
+
+    return uuid;
+}
