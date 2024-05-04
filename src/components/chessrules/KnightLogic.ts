4d1e46e89aefaf49642551f18b1c6f7be8b937dd
Aurotis <jmchu2020@gmail.com>
2024-05-03 21:09:56 -0700

    Added knight premoves

--- src/components/chessrules/KnightLogic.ts
+++ src/components/chessrules/KnightLogic.ts
@@ -1,4 +1,5 @@
 import { ColorTeam } from "../../data/enums/ChessEnums";
+import { ChessPiece } from "../../data/models/ChessPiece";
 import { Position } from "../../data/models/Position";
 import { isSquareOccupied, isSquareOccupiedByOppositeColor } from "./GeneralLogic";
 
@@ -30,4 +31,31 @@ export const knightMove = (initialPosition: Position, newPosition: Position, col
     }
 
     return false;
+}
+
+// Export all possible knight moves from the selected knight piece
+const getPossibleKnightMoves = (knight: ChessPiece, board: ChessPiece[]): Position[] => {
+    // Stores all possible knight moves by piece
+    const possibleMoves: Position[] = [];
+
+    // Checks for all possible knight moves
+    for (let i = -1; i < 2; i+=2) { /* i handles the stem of the L (the long part which is 2 squares long) */
+        for (let j = -1; j < 2; j += 2) {   /* j handles the arm of the L (the short part which is 1 square long) */
+        
+            const verticalMove = new Position(knight.position.x + j, knight.position.y + i * 2);
+            const horizontalMove = new Position(knight.position.x + i * 2, knight.position.y + j);
+  
+            // Check if square is not occupied or occupied by enemy in vertical direction
+            if(!isSquareOccupied(verticalMove, board) || isSquareOccupiedByOppositeColor(verticalMove, board, knight.color)) {
+                possibleMoves.push(verticalMove);
+            }
+  
+            // Check if square is not occupied or occupied by enemy in horizontal direction
+            if(!isSquareOccupied(horizontalMove, board) || isSquareOccupiedByOppositeColor(horizontalMove, board, knight.color))  {
+                possibleMoves.push(horizontalMove);
+            }
+        }           
+    
+    }
+    return possibleMoves;
 }
