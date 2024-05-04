2213e4d283431a46da4aaed62fd6cd9293b6c6b3
Azlothe <bbqian7@gmail.com>
2024-05-04 12:05:11 -0700

    attempt 2pc to prevent data consistency

--- server/database/models/users.mjs
+++ server/database/models/users.mjs
@@ -5,26 +5,38 @@ import constants from "../constants.mjs";
 const saltRounds = 10;
 
 export const insertUser = async (username, email, password) => {
-    // we do not assume the caller has hashed the password; we assume the caller may have passed in plaintext
+  // we do not assume the caller has hashed the password; we assume the caller may have passed in plaintext
 
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  try {
+    await pool.query("BEGIN");
+
     const userAuthResult = await pool.query(
       `INSERT INTO Users (userID, username, email, password) VALUES
-      (uuid_generate_v4(), $1, $2, $3) ON CONFLICT (username) DO NOTHING RETURNING userID;`,
+        (uuid_generate_v4(), $1, $2, $3) ON CONFLICT (username) DO NOTHING RETURNING userID;`,
       [username, email, hashedPassword]
     );
 
     const uuid = userAuthResult.rows[0]?.userid;
-    
+
     if (uuid) {
-        const query = `INSERT INTO ${constants.KEYSPACE}.Users (userID, username, email) VALUES (?, ?, ?);`;
-        await cassandraClient.execute(query, [uuid, username, email], { prepare: true });
+      const query = `INSERT INTO ${constants.KEYSPACE}.Users (userID, username, email) VALUES (?, ?, ?);`;
+      await cassandraClient.execute(query, [uuid, username, email], {
+        prepare: true,
+      });
     }
 
+    await pool.query("COMMIT");
+
     return uuid;
-}
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    console.error("Error: ", error);
+  }
+};
 
 export const getUser = async (username) => {
-    const query = `SELECT username, email FROM ${constants.KEYSPACE}.Users WHERE username = ?;`;
-    return await cassandraClient.execute(query, [username], { prepare: true });
-}
+  const query = `SELECT username, email FROM ${constants.KEYSPACE}.Users WHERE username = ?;`;
+  return await cassandraClient.execute(query, [username], { prepare: true });
+};
