9bcd9687da6c168cfd95af6c8eedee734ee4f5c1
Azlothe <bbqian7@gmail.com>
2024-05-05 23:10:12 -0700

    fix uppercase components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useWhoAmIContext } from "../context/WhoAmIContext";
-import { Button } from "../components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import landscape from "/landscape.png";
