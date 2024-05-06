6817622fd961f203b03f1b4ae8c9e8b31578cd93
Channon Zuo <channon.z2020@gmail.com>
2024-05-05 20:06:22 -0700

    removed comments

--- src/pages/JoinRoom.tsx
+++ src/pages/JoinRoom.tsx
@@ -36,15 +36,3 @@ export function JoinRoom() {
         </main>
     )
 }
-//     return (
-//         <main className = 'h-screen bg-black'>
-//             <NavigationBar />
-//             <h1 className = 'p-2 bg-black text-center w-screen text-3xl font-bold'>
-//                 JOIN ROOM
-//             </h1>
-//             <p>
-//                 PLACEHOLDER
-//             </p>
-//         </main>
-//     )
-// }
