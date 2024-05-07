8f79387673a02c1da85ac573bf86f4655cd0b9da
Azlothe <bbqian7@gmail.com>
2024-05-07 14:24:21 -0700

    add nav items descriptions

--- src/data/constants/NavItems.ts
+++ src/data/constants/NavItems.ts
@@ -3,10 +3,10 @@ const navItems = {
     create: {id: 3, text: 'Create Room', url: '/createroom', desc: 'Create a room'},
     join: {id: 4, text: 'Join Room', url: '/joinroom', desc: 'Join a room'},
     practice: {id: 5, text: 'Practice', url: '/play', desc: 'Play in practice mode'},
-    login: {id: 6, text: 'Login', url: '/login', desc: 'Create a room'},
-    signup: {id: 7, text: 'Sign Up', url: '/signup', desc: 'Create a room'},
-    profile: {id: 8, text: 'Profile', url: '/profile', desc: 'Create a room'},
-    blog: {id: 8, text: 'Blog', url: '/dev', desc: 'Create a room'}
+    login: {id: 6, text: 'Login', url: '/login', desc: 'Log in to your account'},
+    signup: {id: 7, text: 'Sign Up', url: '/signup', desc: 'Sign up for a new account'},
+    profile: {id: 8, text: 'Profile', url: '/profile', desc: 'View your profile'},
+    blog: {id: 8, text: 'Blog', url: '/dev', desc: 'Explore our developer blog'}
 };
 
 
