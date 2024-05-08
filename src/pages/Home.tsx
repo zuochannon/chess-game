de63576ac1d7e270dec6d91d5d007779804785ff
Azlothe <bbqian7@gmail.com>
2024-05-07 18:05:05 -0700

    fix bg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div className="h-screen w-full">
-      <div className="h-full w-full flex flex-col jusimtify-center items-center bg-[url('/public/landscape.png')]">
+    <div className="h-screen w-full bg-[url('/public/landscape.png')] bg-no-repeat">
+      <div className="h-full w-full flex flex-col justify-center items-center">
         <h1>Chess</h1>
         <h3>Online multiplayer chess</h3>
       </div>
