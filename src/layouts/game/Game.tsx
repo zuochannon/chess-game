1ba614dfab18cc7cc7ff2b05288abe228d573a44
Channon Zuo <channon.z2020@gmail.com>
2024-05-10 21:42:42 -0700

    Added onlineHandler to the ChessRulesController parameter
    Allows the ChessRulesController to know who is playing

--- src/layouts/game/Game.tsx
+++ src/layouts/game/Game.tsx
@@ -1,16 +1,17 @@
 import { ColorTeam } from "@/data/enums/ChessEnums";
 import ChessRulesController from "../../components/chessboard/ChessRulesController";
-import "../pages/Play.css"
+import "../pages/Play.css";
 
 interface Props {
     offset: number;
     boardOrientation: ColorTeam;
+    onlineHandler: any;
 }
 
-function Game({ offset, boardOrientation } : Props) {
+function Game({ offset, boardOrientation, onlineHandler } : Props) {
     return (
         <div id = "play" className="content-center">
-            <ChessRulesController offset= {offset} boardOrientation = {boardOrientation} />
+            <ChessRulesController offset= {offset} boardOrientation = {boardOrientation} onlineHandler ={onlineHandler}/>
         </div>
     )
 }
