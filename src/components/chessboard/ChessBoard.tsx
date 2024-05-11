0887d005947d9fab4ab75a50d4767b0f6d85a791
Ahmad Rashdan <ahmrashdanpro@gmail.com>
2024-05-10 17:49:20 -0700

    highlight last move and legal moves

--- src/components/chessboard/ChessBoard.tsx
+++ src/components/chessboard/ChessBoard.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { COLUMNS, ROWS, GRID_SIZE, FULL_SIZE } from "../../data/constants/ChessConstants";
-import "../../layouts/components/Chessboard.css"
+import "../../layouts/components/Chessboard.css";
 import { Position } from "../../data/models/Position";
 import ChessSquare from "./ChessSquare";
 import { ChessPiece } from "../../data/models/ChessPiece";
@@ -13,19 +13,17 @@ interface Props {
     boardOrientation: ColorTeam;
 }
 
-export default function Chessboard({ playMove, pieces, offset, boardOrientation } : Props) {
+export default function Chessboard({ playMove, pieces, offset, boardOrientation }: Props) {
     const chessboardRef = useRef<HTMLDivElement>(null);
     const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
-    const [grabPosition, setGrabPosition] = useState<Position>(new Position(-1,-1));
+    const [grabPosition, setGrabPosition] = useState<Position>(new Position(-1, -1));
+    const [lastMove, setLastMove] = useState<{ from: Position, to: Position } | null>(null);
 
     useEffect(() => {
-        // Add event listener to handle mouse release anywhere on the page
         const handleMouseUp = (e: MouseEvent) => {
-
             if (activePiece && chessboardRef.current) {
                 const isOutsideBoard = !chessboardRef.current.contains(e.target as Node);
                 if (isOutsideBoard) {
-                    // Reset the position of the active piece if dropped outside the board
                     resetPiecePosition();
                 }
             }
@@ -33,118 +31,53 @@ export default function Chessboard({ playMove, pieces, offset, boardOrientation 
 
         document.addEventListener("mouseup", handleMouseUp);
 
-
         return () => {
-            // Clean up the event listener when the component unmounts
             document.removeEventListener("mouseup", handleMouseUp);
         };
     }, [activePiece]);
 
-    // Grabs Piece on board
     function grabPiece(e: React.MouseEvent) {
         const element = e.target as HTMLElement;
         const chessboard = chessboardRef.current;
-
-        // Checks if element is chess piece
         if (element.classList.contains("chess-piece") && chessboard) {
-
-            // Set grab position
             const grabX = Math.floor((e.clientX - chessboard.offsetLeft + window.scrollX) / GRID_SIZE);
             const grabY = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop + window.scrollY - offset - FULL_SIZE) / GRID_SIZE));
-            
-            // Change grab position based on orientation of board
-            if (boardOrientation === ColorTeam.WHITE)
-                setGrabPosition(new Position (grabX, grabY));
-            else
-                setGrabPosition(new Position (7 - grabX, 7 - grabY));
-
-            // Set element position to center of mouse position
-            const x = e.clientX - (GRID_SIZE / 2) + window.scrollX;
-            const y = e.clientY - (GRID_SIZE / 2) - offset + window.scrollY;
-            element.style.position = "absolute";
-            element.style.left = `${x}px`;
-            element.style.top = `${y}px`;
-
-            // Set active piece
+            const adjustedGrabPosition = boardOrientation === ColorTeam.WHITE ? new Position(grabX, grabY) : new Position(7 - grabX, 7 - grabY);
+            setGrabPosition(adjustedGrabPosition);
             setActivePiece(element);
+            element.style.position = "absolute";
+            element.style.left = `${e.clientX - (GRID_SIZE / 2) + window.scrollX}px`;
+            element.style.top = `${e.clientY - (GRID_SIZE / 2) - offset + window.scrollY}px`;
         }
     }
 
-    // Moves Piece on board
     function movePiece(e: React.MouseEvent) {
-        const chessboard = chessboardRef.current;
-
-        // Checks if activePiece and chessboard is not null
-        if (activePiece && chessboard) {
-
-            // Set element position to center of mouse position
-            // Prevents pieces from moving out of board
-            const minX = chessboard.offsetLeft - window.scrollX - (GRID_SIZE / 4);
-            const minY = chessboard.offsetTop - (GRID_SIZE / 4);
-            const maxX = chessboard.offsetLeft + chessboard.clientWidth - ((GRID_SIZE / 4) * 3);
-            const maxY = chessboard.offsetTop + chessboard.clientHeight - ((GRID_SIZE / 4) * 3);
-            const x = e.clientX - (GRID_SIZE / 2) + window.scrollX;
-            const y = e.clientY - (GRID_SIZE / 2) - offset + window.scrollY;
-            activePiece.style.position = "absolute";
-
-            // Set x-position of piece inside board and along mouse cursor
-            if (x < minX) {
-                activePiece.style.left = `${minX}px`;
-            } else if (x > maxX) {
-                activePiece.style.left = `${maxX}px`;
-            } else {
-                activePiece.style.left = `${x}px`;
-            }
-
-            // Set y-position of piece inside board and along mouse cursor
-            if (y < minY) {
-                activePiece.style.top = `${minY}px`;
-            } else if (y > maxY) {
-                activePiece.style.top = `${maxY}px`;
-            } else {
-                activePiece.style.top = `${y}px`;
-            }
-
+        if (activePiece && chessboardRef.current) {
+            const minX = chessboardRef.current.offsetLeft - window.scrollX - (GRID_SIZE / 4);
+            const maxX = chessboardRef.current.offsetLeft + chessboardRef.current.clientWidth - (GRID_SIZE / 4) * 3;
+            const minY = chessboardRef.current.offsetTop - (GRID_SIZE / 4);
+            const maxY = chessboardRef.current.offsetTop + chessboardRef.current.clientHeight - (GRID_SIZE / 4) * 3;
+            const x = Math.max(minX, Math.min(maxX, e.clientX - (GRID_SIZE / 2) + window.scrollX));
+            const y = Math.max(minY, Math.min(maxY, e.clientY - (GRID_SIZE / 2) - offset + window.scrollY));
+            activePiece.style.left = `${x}px`;
+            activePiece.style.top = `${y}px`;
         }
     }
 
-    // Drops piece on board
     function dropPiece(e: React.MouseEvent) {
         const chessboard = chessboardRef.current;
-
-        // Checks if there is a piece and chessboard
         if (activePiece && chessboard) {
             const x = Math.floor((e.clientX - chessboard.offsetLeft + window.scrollX) / GRID_SIZE);
             const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop + window.scrollY - offset - FULL_SIZE) / GRID_SIZE));
-
-            // Get current piece
-            const currentPiece = pieces.find((p) => p.hasSamePositionAs(grabPosition));
-
-            // Checks if current piece is valid
-            if (currentPiece) {
-
-                let successfulMove = false;
-
-                // Change drop orientation based on board
-                if (boardOrientation === ColorTeam.WHITE)
-                    successfulMove = playMove(currentPiece.clone(), new Position(x,y));
-                else {
-                    successfulMove = playMove(currentPiece.clone(), new Position(7 - x, 7 - y));
-                }
-
-                if (!successfulMove) { /* Reset Piece Position */
-                    activePiece.style.position = "relative";
-                    activePiece.style.removeProperty('top');
-                    activePiece.style.removeProperty('left');
-                }
+            const newPosition = boardOrientation === ColorTeam.WHITE ? new Position(x, y) : new Position(7 - x, 7 - y);
+            const currentPiece = pieces.find(p => p.hasSamePositionAs(grabPosition));
+            if (currentPiece && playMove(currentPiece, newPosition)) {
+                setLastMove({ from: grabPosition, to: newPosition });
             }
-
-            // Set active piece to null
-            setActivePiece(null);
+            resetPiecePosition();
         }
     }
 
-    // Reset position of the active piece
     function resetPiecePosition() {
         if (activePiece) {
             activePiece.style.position = "relative";
@@ -156,53 +89,42 @@ export default function Chessboard({ playMove, pieces, offset, boardOrientation 
 
     function renderBoard() {
         const board = [];
-    
-        if (boardOrientation === ColorTeam.WHITE) { /* WHITE BOARD */
-            for (let j = ROWS.length - 1; j >= 0; j--) {
-                for (let i = 0; i < COLUMNS.length; i++) {
-                    const number = j + i + 2;
-                    const piece = pieces.find(p => p.hasSamePositionAs(new Position(i, j)));
-                    let image = piece ? piece.image : undefined;
-        
-                    let currentPiece = activePiece != null ? pieces.find(p => p.hasSamePositionAs(grabPosition)) : undefined;
-                    let highlight = (currentPiece?.possibleMoves) ? currentPiece.possibleMoves.some(p => p.equalsTo(new Position(i, j))) : false;
-
-                    board.push(<ChessSquare key={`${j},${i}`} image={image} number={number} highlight={highlight} />);
-                }
-            }
-        } else { /* BLACK BOARD */
+        for (let j = ROWS.length - 1; j >= 0; j--) {
             for (let i = 0; i < COLUMNS.length; i++) {
-                for (let j = ROWS.length - 1; j >= 0; j--) {
-                    const number = j + i + 2;
-                    const piece = pieces.find(p => p.hasSamePositionAs(new Position(j, i)));
-                    let image = piece ? piece.image : undefined;
-        
-                    let currentPiece = (activePiece != null) ? pieces.find(p => p.hasSamePositionAs(grabPosition)) : undefined;
-                    let highlight = (currentPiece?.possibleMoves) ? currentPiece.possibleMoves.some(p => p.equalsTo(new Position(j, i))) : false;
-        
-                    board.push(<ChessSquare key={`${j},${i}`} image={image} number={number} highlight={highlight} />);
+                const pos = new Position(i, j);
+                const piece = pieces.find(p => p.position.equalsTo(pos));
+                
+                // Determine if this position is part of the last move
+                const isLastMove = lastMove ? (lastMove.from.equalsTo(pos) || lastMove.to.equalsTo(pos)) : false;
+                
+                // Determine if there are possible moves from the currently active piece to this position
+                let isPossibleMove = false;
+                if (activePiece) {
+                    const currentPiece = pieces.find(p => p.position.equalsTo(grabPosition));
+                    isPossibleMove = currentPiece?.possibleMoves?.some(move => move.equalsTo(pos)) ?? false;
                 }
+    
+                // Combine highlight conditions
+                const highlight = isLastMove || isPossibleMove;
+                
+                board.push(<ChessSquare key={`${i},${j}`} image={piece?.image} number={i + j + 2} highlight={highlight} />);
             }
         }
-    
+        
         return board;
     }
     
-    const board = renderBoard();
-
-    //console.log(board);
-
     return (
         <>
-            <div 
-                onMouseDown={(e) => grabPiece(e)}  
-                onMouseMove={(e) => movePiece(e)} 
-                onMouseUp={(e) => dropPiece(e)} 
-                onContextMenu={(e) => e.preventDefault()} // Prevents right-click menu
+            <div
+                onMouseDown={grabPiece}
+                onMouseMove={movePiece}
+                onMouseUp={dropPiece}
+                onContextMenu={(e) => e.preventDefault()}
+                ref={chessboardRef}
                 id="chessboard"
-                ref = {chessboardRef}
             >
-                {board}
+                {renderBoard()}
             </div>
         </>
     );
